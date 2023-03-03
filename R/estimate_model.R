f257cb5edf71f27a02b5ee9b3ec2a378c712901d
damiencrone <damien.crone@gmail.com>
2023-03-03 10:39:08 -0500

    updated estimation method

--- R/estimate_model.R
+++ R/estimate_model.R
@@ -5,6 +5,8 @@
 #'
 #' @param model An OpenMx model object that represents the factor mixture model
 #'   constructed using \code{specify_model}.
+#' @param try_hard Logical indicating whether to use \code{mxTryHard} instead of
+#'   \code{mxRun} for model estimation (default = TRUE).
 #' @param warn Logical indicating whether to suppress warning messages during
 #'   model fitting (default = FALSE).
 #' @param ... Additional arguments passed to \code{mxRun}.
@@ -12,10 +14,14 @@
 #' @return An OpenMx fit object containing the estimated model.
 #'
 #' @import OpenMx
-#' @importFrom OpenMx mxRun imxReportProgress
+#' @importFrom OpenMx mxRun mxTryHard imxReportProgress
 #'
 #' @export
-estimate_model = function (model, warn = FALSE, ...) {
-  fit = OpenMx::mxRun(model, suppressWarnings = warn, ...)
+estimate_model = function (model, try_hard = TRUE, warn = FALSE, ...) {
+  if (try_hard) {
+    fit = OpenMx::mxTryHard(model)
+  } else {
+    fit = OpenMx::mxRun(model, suppressWarnings = warn, ...)
+  }
   return(fit)
 }
