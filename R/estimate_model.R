7d97700554b62a90b67f958fa6416d241aea0c1f
damiencrone <damien.crone@gmail.com>
2023-03-07 11:08:05 -0500

    added extra tries argument

--- R/estimate_model.R
+++ R/estimate_model.R
@@ -7,6 +7,8 @@
 #'   constructed using \code{specify_model}.
 #' @param try_hard Logical indicating whether to use \code{mxTryHard} instead of
 #'   \code{mxRun} for model estimation (default = TRUE).
+#' @param extra_tries The number of additional tries for \code{mxTryHard} to try
+#' to find an acceptable solution (default = 50).
 #' @param warn Logical indicating whether to suppress warning messages during
 #'   model fitting (default = FALSE).
 #' @param ... Additional arguments passed to \code{mxRun}.
@@ -17,9 +19,9 @@
 #' @importFrom OpenMx mxRun mxTryHard imxReportProgress
 #'
 #' @export
-estimate_model = function (model, try_hard = TRUE, warn = FALSE, ...) {
+estimate_model = function (model, try_hard = TRUE, extra_tries = 50, warn = FALSE, ...) {
   if (try_hard) {
-    fit = OpenMx::mxTryHard(model, extraTries = 20)
+    fit = OpenMx::mxTryHard(model, extraTries = extra_tries)
   } else {
     fit = OpenMx::mxRun(model, suppressWarnings = warn, ...)
   }
